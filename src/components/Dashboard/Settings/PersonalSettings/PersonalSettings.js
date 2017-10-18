4f9ae8e823b1e858a00811345e28485d39ddacfe
FredrikOseberg <fredrik.no@gmail.com>
2017-10-18 20:02:39 +1000

    Mobile Settings WIP. CSS changes

--- src/components/Dashboard/Settings/PersonalSettings/PersonalSettings.js
+++ src/components/Dashboard/Settings/PersonalSettings/PersonalSettings.js
@@ -64,7 +64,7 @@ class PersonalSettings extends Component {
 						this.setState({ submitSuccess: 'Your personal information was successfully updated.' }, () => {
 							setTimeout(() => {
 								this.setState({ submitSuccess: '' });
-							}, 100000);
+							}, 5000);
 						});
 					});
 				})
