f903f6dbc0ddf3b1fec72a8fed92e47786bb9d4c
FredrikOseberg <fredrik.no@gmail.com>
2017-11-12 21:19:48 +1300

    Frontend design blog change

--- src/components/Frontend/Blog/BlogPostCard/BlogPostCard.js
+++ src/components/Frontend/Blog/BlogPostCard/BlogPostCard.js
@@ -6,6 +6,9 @@ const BlogPostCard = props => (
 		<img src={props.image} alt="blogpost" />
 		<div className="frontend--blog--post--card--details">
 			<h3>{props.title}</h3>
+			<div className="frontend--blog--post--card--dummy--text--short" />
+			<div className="frontend--blog--post--card--dummy--text--long" />
+			<div className="frontend--blog--post--card--dummy--text--medium" />
 			<p>{props.category}</p>
 		</div>
 	</div>
