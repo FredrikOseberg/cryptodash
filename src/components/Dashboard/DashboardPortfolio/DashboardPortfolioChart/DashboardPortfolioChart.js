31a84c450a49164466d9aba5a69fb55117999b72
FredrikOseberg <fredrik.no@gmail.com>
2017-12-04 21:48:25 +1000

    Converted functional component to class

--- src/components/Dashboard/DashboardPortfolio/DashboardPortfolioChart/DashboardPortfolioChart.js
+++ src/components/Dashboard/DashboardPortfolio/DashboardPortfolioChart/DashboardPortfolioChart.js
@@ -1,47 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
-import { connect } from 'react-redux';
+import { database } from '../../../../firebase';
 import './dashboardportfoliochart.css';
 
-const DashboardPortfolioChart = props => {
-	const data = {
-		labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-		datasets: [
-			{
-				label: 'Portfolio',
-				data: [30000, 37000, 33000, 40000, 42000, 45000, 41000, 42000, 50000],
-				color: '#fff',
-				borderColor: '#fff',
-				pointBorderColor: '#fff',
-				pointBorderFillColor: '#fff',
-				backgroundColor: 'rgba(255,255,255,0.4)',
-				pointBorderRadius: 1.2,
-				fill: true
-			}
-		]
-	};
+class DashboardPortfolioChart extends Component {
+	constructor(props) {
+		super(props);
+	}
 
-	return (
-		<section className="dashboard--portfolio--chart">
-			<header>
-				<div className="dashboard--portfolio--value">
-					<h1>
-						37000<span className="price--postfix">{props.localCurrency.currency}</span>
-					</h1>
-					<p>Portfolio value</p>
-				</div>
-				<div className="dashboard--portfolio--24hr--percentage">
-					<h1>5%</h1>
-					<p>Portfolio 24hr change</p>
-				</div>
-			</header>
-			<Line data={data} />
-		</section>
-	);
-};
+	componentDidMount() {}
 
-const mapStateToProps = state => ({
-	localCurrency: state.localCurrency
-});
+	render() {
+		const data = {
+			labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+			datasets: [
+				{
+					label: 'Portfolio',
+					data: [30000, 37000, 33000, 40000, 42000, 45000, 41000, 42000, 50000],
+					backgroundColor: 'rgba(255,255,255,0.4)',
+					color: '#fff',
+					borderColor: '#fff',
+					pointBorderColor: '#fff'
+				}
+			]
+		};
+		return (
+			<section className="dashboard--portfolio--chart">
+				<header>
+					<div className="dashboard--portfolio--value">
+						<h1>
+							37000<span className="price--postfix" />
+						</h1>
+						<p>Portfolio value</p>
+					</div>
+					<div className="dashboard--portfolio--24hr--percentage">
+						<h1>5%</h1>
+						<p>Portfolio 24hr change</p>
+						<p>Hello</p>
+					</div>
+				</header>
+				<Line data={data} />
+			</section>
+		);
+	}
+}
 
-export default connect(mapStateToProps)(DashboardPortfolioChart);
+export default DashboardPortfolioChart;
